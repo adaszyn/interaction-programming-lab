276a8d94d4ef85fd13896419b3403408600a2737
Shivam Verma <shivam.bitsgoa@gmail.com>
2018-01-22 22:50:20 +0100

    working on dishview

---
+++ js/view/dishView.js
@@ -0,0 +1,21 @@
+function renderMenuItemTemplate(item) {
+    return `
+    <div class="menu-item">
+        <img class="menu-item__image" src="./images/${item.image}" />
+        <h3 class="menu-item__label">${item.name}</h3>
+    </div>
+    `
+}
+
+var DishView = function (container, dish) {
+    var menu = model.getFullMenu()
+    container.find('#guests-number-list').val(model.getNumberOfGuests())
+    var menuContainer = container.find('#menu-container');
+    menu.forEach(function(menuItem) {
+        menuContainer.append(renderMenuItemTemplate(menuItem))
+    })
+    // <div class="menu-item">
+    //                 <img class="menu-item__image" src="./images/bakedbrie.jpg" />
+    //                 <h3 class="menu-item__label">Lasagne</h3>
+    //             </div>
+}
