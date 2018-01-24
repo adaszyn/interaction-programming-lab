9dd3d05232a914b21eee773afeb3a7c957f2f170
Wojciech Adaszyński <wojciech.adaszynski@gmail.com>
2018-01-25 00:32:46 +0100

    SideBar and Header styling with box-shadows, OpenSans added as primary font

--- js/view/plannerView.js
+++ js/view/plannerView.js
@@ -2,8 +2,9 @@ function renderMenuItemTemplate(item) {
     return `
     <a href="#dishView?id=${item.id}" class="menu-item-container">
         <div class="menu-item col-md-3 col-xs-12">
-          <div class="menu-item__image" style="background-image: url('./images/${item.image}')" />
-          <h3 class="menu-item__label">${item.name}</h3>
+          <div class="menu-item__image" style="background-image: url('./images/${item.image}')">
+              <h3 class="menu-item__label">${item.name}</h3>
+          </div>
         </div>
     </a>
     `
