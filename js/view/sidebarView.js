54dc90b70ac450a86b31440e6d40b2dbe5f91df4
Shivam Verma <shivam.bitsgoa@gmail.com>
2018-01-24 19:03:47 +0100

    load data in sidebar from model

--- js/view/sidebarView.js
+++ js/view/sidebarView.js
@@ -1,4 +1,13 @@
 
+function renderTableItemTemplate(dish){
+  return `
+    <tr>
+      <td>${dish.name}</td>
+      <td>${dish.price}</td>
+    </tr>
+  `
+}
+
 var SidebarView = function (container, model) {
     this.container = container;
     this.model = model;
@@ -9,5 +18,13 @@ SidebarView.prototype.render = function() {
   totalPriceElement.text(this.model.getTotalMenuPrice() + " SEK");
 
   var selectMenu = this.container.find('#num-of-guests___select');
-  selectMenu.find("option[value='"+ this.model.numberOfGuests + "']").prop('selected', true);
+  selectMenu
+    .find("option[value='"+ this.model.numberOfGuests + "']")
+    .prop('selected', true);
+
+  var selectedDishesTable = this.container.find('#selected-dishes___table');
+  var selectedDishes = this.model.getFullMenu();
+  $.each(selectedDishes, function(dish){
+    selectedDishesTable.append(renderTableItemTemplate(dish));
+  });
 }
