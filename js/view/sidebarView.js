36d162e418652303d3ab1b7b1c1f0dbd7e2c5864
Shivam Verma <shivam.bitsgoa@gmail.com>
2018-01-24 18:43:49 +0100

    loading data in sidebar from model

--- js/view/sidebarView.js
+++ js/view/sidebarView.js
@@ -1,8 +1,13 @@
 
 var SidebarView = function (container, model) {
-    var totalPriceElement = $('#sidebar-total-price')
-    totalPriceElement.text(model.getTotalMenuPrice() + " SEK") 
+    this.container = container;
+    this.model = model;
 }
+
 SidebarView.prototype.render = function() {
-    
+  var totalPriceElement = this.container.find('#sidebar-total-price');
+  totalPriceElement.text(this.model.getTotalMenuPrice() + " SEK");
+
+  var selectMenu = this.container.find('#num-of-guests___select');
+  selectMenu.find("option[value='"+ this.model.numberOfGuests + "']").prop('selected', true);
 }
